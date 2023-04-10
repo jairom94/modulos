ff090b2ae9ee3e3b7f817902ab7036cf57d1cb34
jairom94 <63824066+jairom94@users.noreply.github.com>
2023-04-10 12:30:41 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import operaciones as op
+def main():
+  print(op.sumar(12,22))
+  print(op.restar(30,14))
+  print(op.multiplicar(4,12))
+  print(op.dividir(125,5))
+
+if __name__ == '__main__':
+  main()
